1c46b887329f7fcc879ee2222f10ae9bd916109e
Pius Aaron <piuschbz@gmail.com>
2025-01-08 09:22:30 +0100

    Add: product review model

---
+++ models/reviews.js
@@ -0,0 +1,31 @@
+// models/reviews.js
+
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = new mongoose.model("Review", reviewSchema);
